e0a5d0fccd200c8562155fffa5848ef309bcdc71
amit6388 <amitpoly2020@gmail.com>
2024-11-24 14:19:09 +0530

    new

---
+++ Mern/Js/Day_8/task.js
@@ -0,0 +1,28 @@
+1. Wap to convert string to uppercase 
+2. Wap to convert string to lowercase 
+3. Wap to Find the Length of a String
+4. Wap to Check if a String Contains a Substring
+5. Wap to Extract a Part of a String
+6. Wap to Replace a Substring
+7. Wap to Split a String into an Array
+8. Wap to Trim space from a String
+9. Wap to  Find the Index of a Substring
+10. What is the syntax to create an object in JavaScript with properties name and age?
+11. How do you access the value of the name property
+12. Write code to add a new property color with value blue
+13. How do you iterate over all the keys in the following object?
+const student = { id: 1, name: "Alice", grade: "A" };
+14. Write a code snippet to convert the string "javascript" to uppercase.
+
+15. How can you check if the string "JavaScript" contains the substring "Script"?
+
+16. What does the spread operator (...) do when used with an array? Provide an example.
+
+17. How do you use the spread operator to copy the following array?
+const fruits = ["apple", "banana", "cherry"];
+
+18. Write code to merge the following two objects into one using the spread operator:
+const obj1 = { a: 1, b: 2 };  
+const obj2 = { c: 3, d: 4 }
+
+19. Create an object person with properties name, age, and address, where address is another object with properties city and zip.
